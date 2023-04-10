217bb49b3843de700844b1b949927e1d200123ac
skh <seogkihong333@gmail.com>
2023-04-10 21:03:45 +0900

    hook 개념

--- src/App.js
+++ src/App.js
@@ -9,30 +9,40 @@ import { Component, createRef, useRef } from "react";
 import IterationSample from "./IterationSample";
 import LifeCycleSample from "./LifeCycleSample";
 import ErrorBoundary from "./ErrorBoundary";
-function getRandomColor() {
-  return "#" + Math.floor(Math.random() * 16777215).toString(16);
-}
-class App extends Component {
-  state = {
-    color: "#000000",
-  };
-  handleClick = () => {
-    this.setState({
-      color: getRandomColor(),
-    });
-  };
+import Info from "./Info";
+import Counter from "./Counter";
+import Average from "./Average";
 
-  render() {
-    return (
-      <div>
-        <button onClick={this.handleClick}>랜덤 색상</button>
-        <ErrorBoundary>
-          <LifeCycleSample color={this.state.color} />
-        </ErrorBoundary>
-      </div>
-    );
-  }
+function App() {
+  return <Average />;
+  // return <Counter />;
+  // return <Info />;
 }
+
+// function getRandomColor() {
+//   return "#" + Math.floor(Math.random() * 16777215).toString(16);
+// }
+// class App extends Component {
+//   state = {
+//     color: "#000000",
+//   };
+//   handleClick = () => {
+//     this.setState({
+//       color: getRandomColor(),
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.handleClick}>랜덤 색상</button>
+//         <ErrorBoundary>
+//           <LifeCycleSample color={this.state.color} />
+//         </ErrorBoundary>
+//       </div>
+//     );
+//   }
+// }
 
 // function App() {
 // return <IterationSample />;
---
+++ src/Info.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import useInputs from "./useInputs";
+
+const Info = () => {
+  const [state, onChange] = useInputs({
+    name: "",
+    nickname: "",
+  });
+  const { name, nickname } = state;
+
+  return (
+    <div>
+      <input value={name} onChange={onChange} />
+      <input value={nickname} onChange={onChange} />
+      <div>
+        <div>
+          <b>이름:</b> {name}
+        </div>
+        <div>
+          <b>닉네임:</b> {nickname}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
